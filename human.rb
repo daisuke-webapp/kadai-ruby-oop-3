2a8bd325157e0018491b921a77c103d0e2179885
daisuke.nagamatsu <mugiblog5656@gmail.com>
2021-06-29 12:44:25 +0000

    first_commit

---
+++ human.rb
@@ -0,0 +1,16 @@
+require './thinkable'
+require './animal'
+
+class Human < Animal
+    
+    attr_accessor :hobby
+        
+    def initialize(name, age, hobby)
+        self.name = name
+        self.age = age
+        self.hobby = hobby
+    end
+    
+    include Thinkable
+
+end
